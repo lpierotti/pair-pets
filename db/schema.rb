d426dcffab1020cf610a663d992a09c54f74ebc7
yuralala8 <yc1435@nyu.edu>
2017-10-12 10:47:32 -0400

    changed db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170804124518) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "breeds", force: :cascade do |t|
     t.string "name"
   end
